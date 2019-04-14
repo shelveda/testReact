13eb8ef54b2fb4c373983276b401b7fa3a447de3
Unknown <saeeddavari72@gmail.com>
2019-04-14 20:58:16 +0430

    starting the page with components

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React from 'react';
+import queryString from 'query-string';
+import App from './../App';
 
-const Posts = () => {
+const Posts = ({ match, location }) => {
+  const { sortBy, approved } = queryString.parse(location.search);
+  console.log(sortBy, approved);
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year:{match.params.year} , Month:{match.params.month}
     </div>
   );
 };
