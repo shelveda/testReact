fba70e3461c44d4dad40ab3b8d7bc8f508ec0959
saeed <dav>
2019-05-14 19:43:24 +0430

    api 2

--- adam/src/App.js
+++ adam/src/App.js
@@ -6,46 +6,59 @@ import Menu from './Menu';
 // import _ from 'lodash';
 
 const DEFAULT_QUERY = 'react';
+const DEFAULT_HPP = '5';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 // const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
-// const isSearched = searchTerm => {
-//   return function(item) {
-//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-//   };
-// };
+const isSearched = searchTerm => {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+};
 
 class App extends Component {
   state = {
-    backup: null,
-    result: null,
-    searchTerm: DEFAULT_QUERY
+    results: null,
+    searchKey: '',
+    searchTerm: DEFAULT_QUERY,
+    searchTermInside: ''
   };
 
   componentDidMount() {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
     fetch(
-      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
   }
   setSearchTopStories = result => {
-    this.setState({ result });
-    this.setState({ backup: result });
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedHits, page } }
+    });
   };
 
   onSearchSubmit = event => {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
     event.preventDefault();
   };
@@ -61,41 +74,45 @@ class App extends Component {
     this.setState({ searchTerm: event.target.value });
   };
 
+  onSearchInside = event => {
+    this.setState({ searchTermInside: event.target.value });
+  };
+
   hide = () => {
     return document.querySelector('#adam').classList.toggle('unvisible');
   };
 
-  reloadStates = () => {
-    const result = { ...this.state.backup };
-    this.setState({ result });
-    console.log(this.state.result);
-  };
-
   testJS = () => {
-    let arr1 = [];
-    arr1 = this.state.result.hits;
-    console.log(typeof arr1);
-    console.log(arr1);
+    const res = {
+      redux: { hits: [{ name: 'saeed' }, { name: 'saeed2' }], page: 2 }
+    };
 
-    const arr2 = [
-      { author: 'dwwoelfel', comment_text: null },
-      { author: 'peterhunt', comment_text: null }
-    ];
-    const arr3 = [...arr1, ...arr2];
-    // console.log(arr1);
-    console.log(arr3);
+    const name = 'react';
+    const hits2 = [{ name: 'mykel' }, { name: 'me2' }];
+
+    const restfull = { ...res, [name]: { hits2 } };
+    console.log(restfull);
   };
 
   render() {
-    const { searchTerm, result } = this.state;
-    if (!result) {
-      return <Menu />;
+    const { searchTerm, results, searchKey, searchTermInside } = this.state;
+
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
+
+    if (!results) {
+      return (
+        <div>
+          <button onClick={this.testJS}>testJS </button>
+          <Menu />
+        </div>
+      );
     }
     return (
       <div className="page">
-        <Button onClick={() => this.reloadStates()} className="button-active ">
-          reload No Ajax
-        </Button>
         <Button onClick={this.hide} className="button-inline">
           show
         </Button>
@@ -108,11 +125,25 @@ class App extends Component {
             >
               search
             </Search>
+            <Search value={searchTermInside} onChange={this.onSearchInside} />
           </div>
-          <Table list={result.hits} onDismiss={this.onDismiss} />
+
+          <Table
+            list={list}
+            onDismiss={this.onDismiss}
+            pattern={searchTermInside}
+          />
+
+          <div className="interactions">
+            <Button
+              onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+            >
+              More
+            </Button>
+          </div>
         </div>
-        <Menu />
         <button onClick={this.testJS}>run me </button>
+        <Menu />
       </div>
     );
   }
@@ -127,18 +158,18 @@ const Search = ({ value, onChange, children, onSubmit }) => (
 
 class Table extends Component {
   render() {
-    const { list, onDismiss } = this.props;
+    const { list, onDismiss, pattern } = this.props;
     return (
       <div className="table">
-        {list.map(item => (
+        {list.filter(isSearched(pattern)).map(item => (
           <div key={item.objectID} className="table-row">
-            <span style={{ width: '40%' }}>
+            <span style={{ width: '50%' }}>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span style={{ width: '30%' }}> {item.author} </span>
-            <span style={{ width: '5%' }}> {item.num_comments} </span>
-            <span style={{ width: '5%' }}> {item.points} </span>
-            <span style={{ width: '20%' }}>
+            <span style={{ width: '20%' }}> {item.author} </span>
+            <span style={{ width: '10%' }}> {item.num_comments} </span>
+            <span style={{ width: '10%' }}> {item.points} </span>
+            <span style={{ width: '10%' }}>
               <Button
                 onClick={() => onDismiss(item.objectID)}
                 className="button-inline"
